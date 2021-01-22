b265d8ea788874040681f4ddc53125be24c715fc
Saimun Hossain <saimunhossaiemon@gmail.com>
2021-01-22 16:46:38 +0600

    all file done

---
+++ assignment.js
@@ -0,0 +1,60 @@
+// kilometerToMeter
+// budgetCalculator 
+// hotelCost
+// megaFriend
+
+// function kilometerToMeter(kilo) {
+//     var kilometer = kilo / 1000;
+//     return kilometer;
+// }
+// var result = kilometerToMeter(23456);
+//  console.log(result);
+
+
+// function budgetCalculator(watch, mobile, laptop) {
+//     var wathcForMoney = watch * 4;
+//     var mobileForMoney = mobile * 6;
+//     var laptopForMoney = laptop * 8;
+//     var allShopForMoney = wathcForMoney + mobileForMoney + laptopForMoney;
+//     return allShopForMoney;
+// }
+// var result = budgetCalculator(50, 100, 500);
+// console.log(result);
+
+
+//  hotelCost
+function hotelCost(first, second, third) {
+    var firstDay = first * 10;
+    var secondDay = second * 8;
+    var thirdDay = third * 6;
+    var allDay = firstDay + secondDay + thirdDay;
+    return allDay;
+
+}
+var result = hotelCost(32, 25, 48);
+console.log(result);
+
+
+
+// megaFriend
+var name = ["salam", "kalam", "nalam", "saimunhossain"];
+function megaFriend(name) {
+    var megaLength = 0;
+    var friendName = [];
+    for (var i = 0; i < name.length; i++) {
+        if (megaLength < name[i].length) {
+            megaLength = name[i].length;
+        }
+    }
+    for (var p = 0; p < name.length; p++) {
+        if (name[p].length == megaLength) {
+            friendName.push(name[p]);
+        }
+    }
+    if (friendName.length == 1) {
+        return friendName[0];
+    }
+    else {
+        return friendName;
+    }
+}
